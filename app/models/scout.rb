6a18772dbab97329dd2328165e92d4747ed5779c
Carson Cole <carson.cole@nuku.com>
2016-09-06 20:48:59 -0700

    added parent name to scout show

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -23,6 +23,10 @@ class Scout < ApplicationRecord
     first_name + ' ' + last_name
   end
 
+  def parent_name
+    (parent_first_name||'') + ' ' + (parent_last_name||'')
+  end
+
   def total_sales
     total = 0
     take_orders.each do |to|
