6bb5ea6f8c0a2ec608c2780b5ac19abbf27772a3
Carson Cole <carson.cole@nuku.com>
2016-09-10 13:42:11 -0700

    fix

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -18,10 +18,6 @@ class PrizesController < ApplicationController
     @prize = Prize.new
   end
 
-  def name_with_id
-    name + " (" + id.to+s + ")" 
-  end
-
   # GET /prizes/1/edit
   def edit
   end
