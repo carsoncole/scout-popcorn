799f0b17b216f94582c74458dc6f3881d1dff846
Carson Cole <carson.cole@nuku.com>
2016-09-11 14:50:26 -0700

    added sales breakdown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @events = @unit.events.active
+    @pack_donations = SiteSaleLineItem.joins(:product, :site_sale).where("site_sales.event_id = ?", @active_event.id).where("products.name = ?",'Pack Donation').sum(:value)
+    @product_sales = SiteSaleLineItem.joins(:product, :site_sale).where("site_sales.event_id = ?", @active_event.id).where("products.name <> ?",'Pack Donation').sum(:value)
   end
 
 end
