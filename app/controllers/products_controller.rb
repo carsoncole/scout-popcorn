3d46dc96878e70055d5863c28e39687a584c2ddf
Carson Cole <carson.cole@nuku.com>
2016-09-18 18:38:23 -0700

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,6 +74,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :quantity, :retail_price, :short_name)
+      params.require(:product).permit(:name, :quantity, :retail_price, :short_name, :url)
     end
 end
