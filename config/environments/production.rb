e9d6a0745a47d7237a6ce2db95a4ad46199ea86a
Carson Cole <carson.cole@nuku.com>
2016-09-28 06:55:10 -0700

    fix for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.action_mailer.default_url_options = { :host => "corncub.us" }
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
