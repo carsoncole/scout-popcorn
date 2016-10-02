70740332bad74b5938e593fd5ecb032ef7400a38
Carson Cole <carson.cole@nuku.com>
2016-10-02 13:26:56 -0700

    fix

--- app/controllers/scout_prize_selections_controller.rb
+++ app/controllers/scout_prize_selections_controller.rb
@@ -2,7 +2,7 @@ class ScoutPrizeSelectionsController < ApplicationController
   def index
     @scout_prizes = @active_event.scout_prizes
     scout_ids_with_prizes = @scout_prizes.map{|sp| sp.scout_id}.uniq
-    @scouts_without_prizes = @unit.scouts.map {|scout| scout if scout.total_sales(@active_event) > 0 && !scout_ids_with_prizes.include?(scout.id) }
+    @scouts_without_prizes = @unit.scouts.map {|scout| scout if scout.total_sales(@active_event) > 0 && !scout_ids_with_prizes.include?(scout.id) }.compact
   end
 
 end
