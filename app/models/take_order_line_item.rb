674ec2725f2ede91f39edb9769f1bf01142f4bb9
Carson Cole <carson.cole@nuku.com>
2016-10-10 21:08:52 -0700

    added ledger_id to ledgers

--- app/models/take_order_line_item.rb
+++ app/models/take_order_line_item.rb
@@ -1,6 +1,7 @@
 class TakeOrderLineItem < ApplicationRecord
   belongs_to :take_order
   belongs_to :product
+  belongs_to :ledger, optional: true
   validates :product_id, :quantity, presence: true
   before_save :update_value!
 
