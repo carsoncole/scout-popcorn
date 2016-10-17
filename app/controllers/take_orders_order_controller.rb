664a0dfe33e98e5d7b9e50a96115bdc3a77a24a3
Carson Cole <carson.cole@nuku.com>
2016-10-17 08:48:42 -0700

    added order

--- app/controllers/take_orders_order_controller.rb
+++ app/controllers/take_orders_order_controller.rb
@@ -1,6 +1,6 @@
 class TakeOrdersOrderController < ApplicationController
   def index
     @submitted_take_orders = @active_event.take_orders.submitted
-    @products_to_order = @submitted_take_orders.includes(take_order_line_items: :product).group(:product_id).sum('take_order_line_items.quantity')
+    @products_to_order = @submitted_take_orders.includes(take_order_line_items: :product).order('products.name').group(:product_id).sum('take_order_line_items.quantity')
   end
 end
