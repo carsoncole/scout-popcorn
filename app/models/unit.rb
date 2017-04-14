3d28fd4cbcc148dbedf2324405a5ff9df50a738a
Carson Cole <carson.cole@nuku.com>
2017-04-14 15:54:39 -0700

    added test

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,12 +1,7 @@
 class Unit < ApplicationRecord
   has_many :scouts, dependent: :destroy
-  has_many :prizes, through: :events
   has_many :payment_methods
   has_many :events, dependent: :destroy
-  has_many :take_orders, through: :scouts
-  has_many :site_sales, through: :scouts
-  has_many :purchase_orders, through: :events
-  has_many :take_order_purchase_orders
   has_many :ledgers, through: :accounts
 
   validates :name, presence: true
--- test/models/scout_test.rb
+++ test/models/scout_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ScoutTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without first_name" do
+    scout = Scout.new
+    assert_not scout.save
+    assert scout.errors[:first_name]
+  end
+
+  test "should not save without last_name" do
+    scout = Scout.new
+    assert_not scout.save
+    assert scout.errors[:last_name]
+  end
+
+  test "should not save without email" do
+    scout = Scout.new
+    assert_not scout.save
+    assert scout.errors[:email]
+  end
 end
