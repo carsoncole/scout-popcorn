b6ab1c27c7667c04a069ff953ef94e03d17da34d
Carson Cole <carson.cole@nuku.com>
2017-05-19 20:42:24 -0700

    removed password, outdated info

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.delivery_method = :smtp
 
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  authentication: :login,
-  enable_starttls_auto: true,
-  user_name: "carson.cole@gmail.com",
-  password: "GChiLanM67$$"
- }
+# ActionMailer::Base.smtp_settings = {
+#   address: "smtp.gmail.com",
+#   port: 587,
+#   domain: "gmail.com",
+#   authentication: :login,
+#   enable_starttls_auto: true,
+#   user_name: "name@gmail.com",
+#   password: "password"
+#  }
