3abfe3399ee7743696b19ac13e612a26cdf669cd
Carson Cole <carson.cole@nuku.com>
2017-05-22 16:31:21 -0700

    minor

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -6,31 +6,31 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     @event = events(:one)
   end
   
-  test "should not get index without sign_in" do
+  test 'should not get index without sign_in' do
     get events_url
     assert_redirected_to controller: 'sessions', action: 'new'
   end
 
-  test "should not get edit for non-logins" do
+  test 'should not get edit for non-logins' do
     get edit_event_url(@event)
     assert_response :redirect
     assert_redirected_to root_url
   end
 
-  test "should not get index" do
+  test 'should not get index' do
     sign_in(scouts(:one))
     get events_url
     assert_response :redirect
     assert_redirected_to home_path
   end
 
-  test "should not get new" do
+  test 'should not get new' do
     sign_in(scouts(:one))
     get new_event_url
     assert_redirected_to controller: 'home', action: 'index'
   end
 
-  test "should not create event" do
+  test 'should not create event' do
     sign_in(scouts(:one))
     assert_difference('Event.count', 0) do
       post events_url, params: { event: { name: @event.name, unit_id: @event.unit_id } }
@@ -39,25 +39,25 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to home_url
   end
 
-  test "should not show event" do
+  test 'should not show event' do
     sign_in(scouts(:one))
     get event_url(@event)
     assert_redirected_to home_url
   end
 
-  test "should not get edit" do
+  test 'should not get edit' do
     sign_in(scouts(:one))
     get edit_event_url(@event)
     assert_redirected_to home_url
   end
 
-  test "should not update event" do
+  test 'should not update event' do
     sign_in(scouts(:one))
     patch event_url(@event), params: { event: { name: @event.name, unit_id: @event.unit_id } }
     assert_redirected_to home_url
   end
 
-  test "should not destroy event" do
+  test 'should not destroy event' do
     sign_in(scouts(:one))
     assert_difference('Event.count', 0) do
       delete event_url(@event)
@@ -68,26 +68,26 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
 
   # Admin
 
-  test "should get index" do
+  test 'should get index' do
     sign_in(scouts(:unit_admin))
     get events_url
     assert_response :success
   end
 
-  test "should not get index for other admins" do
+  test 'should not get index for other admins' do
     sign_in(scouts(:take_orders_admin))
     get events_url
     assert_response :redirect
     assert_redirected_to home_path
   end
 
-  test "should get new" do
+  test 'should get new' do
     sign_in(scouts(:unit_admin))
     get new_event_url
     assert_response :success
   end
 
-  test "should create event" do
+  test 'should create event' do
     sign_in(scouts(:unit_admin))
     assert_difference('Event.count', 1) do
       post events_url, params: { event: { name: @event.name, unit_id: @event.unit_id } }
@@ -96,25 +96,25 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to events_url
   end
 
-  test "should show event" do
+  test 'should show event' do
     sign_in(scouts(:unit_admin))
     get event_url(@event)
     assert_redirected_to events_url
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     sign_in(scouts(:unit_admin))
     get edit_event_url(@event)
     assert_response :success
   end
 
-  test "should update event" do
+  test 'should update event' do
     sign_in(scouts(:unit_admin))
     patch event_url(@event), params: { event: { name: @event.name, unit_id: @event.unit_id } }
     assert_redirected_to events_url
   end
 
-  test "should destroy event" do
+  test 'should destroy event' do
     sign_in(scouts(:unit_admin))
     assert_difference('Event.count', -1) do
       delete event_url(@event)
@@ -123,7 +123,7 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to events_url
   end
 
-  test "should not get edit for other admins" do
+  test 'should not get edit for other admins' do
     sign_in(scouts(:take_orders_admin))
     get edit_event_url(@event)
     assert_response :redirect
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "minitest/rails/capybara"
+require 'minitest/rails/capybara'
 require 'application_helper'
 
 class ActiveSupport::TestCase
