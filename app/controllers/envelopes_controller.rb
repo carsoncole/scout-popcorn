1c7e181091adc7fa836d4929de9b61d04d38b473
Carson Cole <carson.cole@gmail.com>
2017-10-10 23:07:51 -0700

    fix

--- app/controllers/envelopes_controller.rb
+++ app/controllers/envelopes_controller.rb
@@ -23,7 +23,7 @@ class EnvelopesController < ApplicationController
 
   def show
     @take_orders = @envelope.take_orders.order(:created_at)
-    @payment_methods = @take_orders.uniq.pluck(:payment_account_id)
+    @payment_methods = @take_orders.pluck(:payment_account_id).uniq
   end
 
   def new
